5e75d07c8efbc2cddc925410b3956e5951f31e26
内田正輝 <masaki.u@arsaga.jp>
2022-05-09 17:14:23 +0900

    ボタンの内容を列挙しやすくなる関数を作成

--- presentation/layout/core/src/main/java/jp/arsaga/presentation/layout/core/widget/ButtonStyle.kt
+++ presentation/layout/core/src/main/java/jp/arsaga/presentation/layout/core/widget/ButtonStyle.kt
@@ -1,4 +1,31 @@
 package jp.arsaga.presentation.layout.core.widget
 
-enum class ButtonStyle {
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+
+enum class ButtonStyle(
+    val style: TextStyle,
+    val maxLines: Int = 1
+) {
+}
+
+@Composable
+fun RowScope.ButtonContent(
+    text: String,
+    buttonStyle: ButtonStyle,
+    modifier: Modifier = Modifier,
+    leftIcon: @Composable RowScope.() -> Unit,
+    rightIcon: @Composable RowScope.() -> Unit,
+) {
+    leftIcon(this)
+    Text(
+        text = text,
+        maxLines = buttonStyle.maxLines,
+        style = buttonStyle.style,
+        modifier = modifier
+    )
+    rightIcon(this)
 }
