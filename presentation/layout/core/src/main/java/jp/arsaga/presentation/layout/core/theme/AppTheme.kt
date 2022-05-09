ed459f18e6e008b33fd7ffd2473950e3dc79d113
内田正輝 <masaki.u@arsaga.jp>
2022-05-09 19:21:48 +0900

    マテリアルテーマをダークモードに応じて出し分け

--- presentation/layout/core/src/main/java/jp/arsaga/presentation/layout/core/theme/AppTheme.kt
+++ presentation/layout/core/src/main/java/jp/arsaga/presentation/layout/core/theme/AppTheme.kt
@@ -1,17 +1,20 @@
 package jp.arsaga.presentation.layout.core.theme
 
+import android.content.Context
+import android.content.res.Configuration
 import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Shapes
 import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun AppTheme(
     content: @Composable () -> Unit
 ) {
     MaterialTheme(
-        colors = AppTheme.colors(),
+        colors = AppTheme.colors(LocalContext.current),
         typography = AppTheme.typography(),
         shapes = AppTheme.shapes(),
         content = content
@@ -19,10 +22,6 @@ fun AppTheme(
 }
 
 object AppTheme {
-    @Composable
-    fun colors(): Colors = MaterialTheme.colors.copy(
-
-    )
 
     @Composable
     fun typography(): Typography = MaterialTheme.typography.copy(
@@ -33,4 +32,20 @@ object AppTheme {
     fun shapes(): Shapes = MaterialTheme.shapes.copy(
 
     )
+
+    @Composable
+    fun colors(context: Context): Colors =
+        if (context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES)
+            AppThemeMode.Dark.colors()
+        else AppThemeMode.Day.colors()
+}
+
+enum class AppThemeMode(
+    val colors: @Composable () -> Colors = {
+        MaterialTheme.colors.copy(
+
+        )
+    }
+) {
+    Day, Dark
 }
