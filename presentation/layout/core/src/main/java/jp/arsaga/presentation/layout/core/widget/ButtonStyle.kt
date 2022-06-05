581dea0701cd26cec2c73cad9df19dc0d0f337ca
内田正輝 <masaki.u@arsaga.jp>
2022-06-05 11:35:01 +0900

    buttonContent関数を階層化

--- presentation/layout/core/src/main/java/jp/arsaga/presentation/layout/core/widget/ButtonStyle.kt
+++ presentation/layout/core/src/main/java/jp/arsaga/presentation/layout/core/widget/ButtonStyle.kt
@@ -18,13 +18,24 @@ fun buttonContent(
     modifier: Modifier = Modifier,
     leftIcon: @Composable RowScope.() -> Unit = {},
     rightIcon: @Composable RowScope.() -> Unit = {},
-): @Composable RowScope.() -> Unit = {
-    leftIcon(this)
+): @Composable RowScope.() -> Unit = buttonContent(
+    leftIcon = leftIcon,
+    rightIcon = rightIcon
+) {
     Text(
         text = text,
         maxLines = buttonStyle.maxLines,
         style = buttonStyle.style,
         modifier = modifier
     )
+}
+
+fun buttonContent(
+    leftIcon: @Composable RowScope.() -> Unit = {},
+    rightIcon: @Composable RowScope.() -> Unit = {},
+    centerIcon: @Composable RowScope.() -> Unit = {},
+): @Composable RowScope.() -> Unit = {
+    leftIcon(this)
+    centerIcon(this)
     rightIcon(this)
 }
